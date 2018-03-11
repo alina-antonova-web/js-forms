6db68ff965cc4ea6bd5db188d6ec291ee2e5ae92
Alina <maliniaka.web@gmail.com>
2018-03-11 11:08:23 +0100

    Login form validation is ready

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
 	//console.log('Hello world!');
+
+	// Validate comment form
 	(function(){
 		var formCommentValidation = {
 			init: function(){
@@ -49,6 +51,90 @@ $(document).ready(function() {
 		};
 
 		formCommentValidation.init();
+	}());
+	// End Validate comment form
+
+
+	(function(){
+
+		var formLoginValidation = {
+
+			isValid: true,
+
+			init: function(){
+				// вызов внутренних функций
+				this._setUpListeners();
+			},
+
+			_setUpListeners: function(){
+				$('#submitLoginForm').on('click', formLoginValidation._validateForm );
+
+				var inputs = $("#loginForm").find('input');
+				$.each(inputs, function(index, val){
+					var input = $(val);
+					// Hide errors
+					input.on('focus', function(){
+						$(".show-error").removeClass('show-error');
+					});
+					input.on('keydown', function(){
+						$(".show-error").removeClass('show-error');
+					});
+					input.on('change', function(){
+						$(".show-error").removeClass('show-error');
+					});	
+				});
+
+				
+			},
+
+			_validateForm: function(event){
+				event.preventDefault();
+				var form = $("#loginForm"),
+					email = $("#email").val().trim(),
+					password = $("#password").val().trim(),
+					validEmail = "mail@mail.com",
+					validPassword = "123",
+					pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
+					valid = true;
+
+				//Check email
+				if ( email.length === 0 ) {
+					// Show errors
+					$("#emptyEmail").addClass('show-error');
+					console.log('Email is empty');
+					valid = false;
+				} else if ( !pattern.test( email ) ) {
+					// Show errors
+					$("#errorEmail").addClass('show-error');
+					console.log('Email is INVALID');
+					valid = false;
+  				} else {
+  					console.log('Email is VALID');
+  					$(".show-error").removeClass('show-error');
+  					//Check password
+					if ( password.length === 0 ) {
+						// Show errors
+						$("#emptyPassword").addClass('show-error');
+						console.log('Password is empty');
+						valid = false;
+					} else if ( email !== validEmail || password !== validPassword ) {    //Check valid Email and password
+						// Show errors
+						$(".wrongEmailPass").addClass('show-error');
+						console.log('Wrong email or password');
+						valid = false;
+					}
+				}		
+
+				
+				if ( valid === true ) {
+					console.log('Sending form!');
+					form.submit();
+				}
+			}
+		};
+
+		formLoginValidation.init();
+
 	}());
 
 });
